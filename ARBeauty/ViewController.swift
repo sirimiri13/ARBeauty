d0c9495a871fa024a70bc3b9bb43b6aeee5afc23
Lam Huong <lamquynhhuong1304@gmail.com>
2021-02-19 16:17:25 +0700

    Layout MainView

--- ARBeauty/ViewController.swift
+++ ARBeauty/ViewController.swift
@@ -9,12 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nailsButton: UIButton!
+    @IBOutlet weak var lipsButton: UIButton!
+    @IBOutlet weak var galleryButton: UIButton!
+    
+    @IBOutlet weak var buttonStackView: UIStackView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Test")
-        // Do any additional setup after loading the view.
+        setView()
     }
 
-
+    func setView(){
+        nailsButton.setTitle("NAILS", for: .normal)
+        lipsButton.setTitle("LIPS", for: .normal)
+        galleryButton.setTitle("GALLERY", for: .normal)
+        let backgroundImg = UIImageView()
+        backgroundImg.image = UIImage(named: "background")
+        self.view.addSubview(backgroundImg)
+        backgroundImg.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            backgroundImg.topAnchor.constraint(equalTo: view.topAnchor),
+            backgroundImg.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            backgroundImg.leftAnchor.constraint(equalTo: view.leftAnchor),
+            backgroundImg.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+        
+        backgroundImg.addSubview(nameLabel)
+        backgroundImg.addSubview(buttonStackView)
+        
+    }
 }
 
