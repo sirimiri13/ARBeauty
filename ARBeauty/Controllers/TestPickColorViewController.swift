b942d596a223ebd28d95c80689de21e510dfff76
Huong Lam <huong.lam@devsamurai.com>
2021-04-02 17:11:59 +0700

    fix Pick Color

--- ARBeauty/Controllers/TestPickColorViewController.swift
+++ ARBeauty/Controllers/TestPickColorViewController.swift
@@ -15,18 +15,18 @@ class TestPickColorViewController: UIViewController {
     
     var location = CGPoint(x: 0.0, y: 0.0)
     var deltaY = CGFloat()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-     
+        
         imageView.addSubview(self.colorView)
         imageView.bringSubviewToFront(self.colorView)
-    
+        
         colorView.center = imageView.center
         let color: UIColor = (imageView.image?.getPixelColor(pos: CGPoint(x: imageView.center.x, y: imageView.center.y)))!
         colorView.image = UIImage(named: "color-picker")?.withTintColor(color)
-       // colorView.image = colorView.image?.drawOutlie(color: UIColor.black)
-   
+        // colorView.image = colorView.image?.drawOutlie(color: UIColor.black)
+        
     }
     
     
@@ -38,17 +38,19 @@ class TestPickColorViewController: UIViewController {
             let color: UIColor = (imageView.image?.getPixelColor(pos: CGPoint(x: location.x, y: location.y)))!
             colorView.center = CGPoint(x: location.x ,y: location.y - deltaY)
             colorView.image = UIImage(named: "color-picker")?.withTintColor(color)
-          //  colorView.image = colorView.image?.drawOutlie(color: UIColor.black)        }
+            //  colorView.image = colorView.image?.drawOutlie(color: UIColor.black)        }
+        }
     }
-    
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first{
-            location = touch.location(in: imageView)
-            let color: UIColor = (imageView.image?.getPixelColor(pos: CGPoint(x: location.x, y: location.y)))!
-            deltaY = colorView.frame.height * 0.5
-            colorView.center = CGPoint(x: location.x, y: location.y - deltaY)
-            colorView.image = UIImage(named: "color-picker")?.withTintColor(color)
-         //   colorView.image = colorView.image?.drawOutlie(color: UIColor.black)        }
+        
+        
+        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+            if let touch = touches.first{
+                location = touch.location(in: imageView)
+                let color: UIColor = (imageView.image?.getPixelColor(pos: CGPoint(x: location.x, y: location.y)))!
+                deltaY = colorView.frame.height * 0.5
+                colorView.center = CGPoint(x: location.x, y: location.y - deltaY)
+                colorView.image = UIImage(named: "color-picker")?.withTintColor(color)
+                //   colorView.image = colorView.image?.drawOutlie(color: UIColor.black)        }
+            }
+        }
     }
-}
